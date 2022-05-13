ddb7cade5df7d46fd5f639d1dd9ee84c16715cbc
MiissSheep <lea.makil@efrei.net>
2022-05-13 09:55:23 +0200

    commit on Dev_improve

---
+++ MVS_FizzBuzz.h
@@ -0,0 +1,9 @@
+//
+// Created by cmoil on 13/05/2022.
+//
+
+#ifndef UNTITLED1_MVS_FIZZBUZZ_H
+#define FIZZ "Fizz"
+#define BUZZ "Buzz"
+
+#endif //UNTITLED1_MVS_FIZZBUZZ_H
--- main.c
+++ main.c
@@ -1,17 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-#define FIZZ "Fizz"
-#define BUZZ "Buzz"
-
+#include "MVS_FizzBuzz.h"
 int main (void) {
     for (int i = 1; i <= 100; i++) {
-        if (i % 15 == 0)
-        {
+        if (i % 15 == 0) {
             printf ("%s%s\n", FIZZ, BUZZ);
-        }
-        else if (i % 3 == 0)
-        {
+        } else if (i % 3 == 0) {
             printf ("%s\n", FIZZ);
         } else if (i % 5 == 0) {
             printf ("%s\n", BUZZ);
@@ -19,6 +13,5 @@ int main (void) {
             printf ("%d\n", i);
         }
     }
-
     return EXIT_SUCCESS;
 }
